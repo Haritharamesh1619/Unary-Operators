f03c7a0c7e91af2e2678660c8b5c43b8a9cc318b
Haritha R <harithalaks1998@gmail.com>
2024-06-27 13:56:05 +0530

    Add project files.

---
+++ Unary Operators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unary_Operators
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            int ans = a++; // post - increment example: 
+            Console.WriteLine("a is {0} and ans is {1}",a,ans);
+            ans = a--;// post-decrement example:
+            Console.WriteLine("a is {0} and ans is {1}", a, ans);
+            ans = ++a;// pre-decrement example:
+            Console.WriteLine("a is {0} and ans is {1}", a, ans);
+            ans = --a;// pre-decrement example:
+            Console.WriteLine("a is {0} and ans is {1}", a, ans);
+            Console.ReadLine();
+        }
+    }
+}
